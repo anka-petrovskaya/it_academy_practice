6bdb43e5bd2024e7f05d5d1948ea409845ae9378
Hanna Piatrouskaya <anka.petrovskaya@gmail.com>
2021-02-20 09:55:54 +0300

    added task 2

---
+++ LettersCount/LettersCount/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LettersCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.WriteLine("Enter your word");
+            string yourword = Console.ReadLine();
+
+            for (int i = 0; i < yourword.Length; ++i)
+                   if (yourword[i] == 'a')
+                     count++;
+      
+               Console.WriteLine(count);
+        }
+    } 
+}
