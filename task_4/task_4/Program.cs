ca79ea05c7c9f4ad5929ff3330a7f4e0c3e54a4f
Hanna Piatrouskaya <anka.petrovskaya@gmail.com>
2021-02-22 21:21:50 +0300

    added 4

---
+++ task_4/task_4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            short number = 5;
+            object  o = number;
+
+            sbyte number1 = (sbyte)(short)o;
+            Console.WriteLine("Variable type: " + number.GetType());
+            Console.WriteLine("Variable type: " + number1.GetType());
+        }
+    }
+}
