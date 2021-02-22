004deae6094a1f8cd1af2b5d98fefc9b9040b10b
Hanna Piatrouskaya <anka.petrovskaya@gmail.com>
2021-02-22 20:47:13 +0300

    added task 2

---
+++ task2/task2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 32 };
+            char[] symbs = new char[] { 'A' };
+            string[] txts = new string[] { "Hello" };
+
+            Console.WriteLine("Massive nums:");
+
+            for (int k=0; k<nums.Length; k++)
+            {
+                Console.Write(nums[k] + " ");
+            }
+            Console.WriteLine("\nMassive symbs:");
+
+            for (int k = 0; k < symbs.Length; k++)
+            {
+                Console.Write(symbs[k] + " ");
+            }
+            Console.WriteLine("\nMassive txts:");
+
+            for (int k = 0; k < txts.Length; k++)
+            {
+                Console.Write(txts[k] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
