951ecef31f87990f49a0c8e45c1c253ec89d276b
Hanna Piatrouskaya <anka.petrovskaya@gmail.com>
2021-02-22 20:37:32 +0300

    added task 1

---
+++ practices_tasks/practices_tasks/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace practices_tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("As I understand it is better to user collections when you are not sure about the size of your massive.");
+           
+        }
+    }
+}
