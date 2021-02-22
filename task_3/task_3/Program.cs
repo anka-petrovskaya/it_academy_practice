15519ac50d604c8c21649a6ed7a88d8c361affa7
Hanna Piatrouskaya <anka.petrovskaya@gmail.com>
2021-02-22 22:26:53 +0300

    added correct task 3

--- task_3/task_3/Program.cs
+++ task_3/task_3/Program.cs
@@ -6,30 +6,28 @@ namespace task_3
     {
         static void Main(string[] args)
         {
+            int value, number;
+            int size = 13;
             Random rnd = new Random();
-            int size = 10, k;
             int[] nums = new int[size];
             Console.WriteLine("Массив случайных чисел!");
 
-            for (k = 0; k < nums.Length; k++)
+            for (int k = 0; k < nums.Length; k++)
             {
-                nums[k] = (int)('A' + rnd.Next(26));
+                nums[k] = (int)('A' + rnd.Next(1,101));
                 Console.Write("| " + nums[k] + " ");
             }
             Console.Write("|");
-
-            Console.WriteLine();
-            int value, number;
+            
             number = 0;
             value = nums[number];
 
-            for (int i = 1; i < nums.Length; i++)
+            for (int k = 1; k < nums.Length; k++)
             {
-                if (nums[i]>value)
+                if (nums[k]>value)
                 {
                     value = nums[k];
-                    number k;
-
+                    number=k;
                 }
             }
             Console.WriteLine("наибольшее значение: " + value);
