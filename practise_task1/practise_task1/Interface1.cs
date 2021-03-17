fc100f8dca4bc271a07a477b070be4044db5c408
Hanna Piatrouskaya <anka.petrovskaya@gmail.com>
2021-03-17 19:54:54 +0300

    added pr_12

---
+++ practise_task1/practise_task1/Interface1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practise_task1
+{
+    interface Iinterface1
+    {
+        public void CircleSquare()
+        {
+        }
+
+        public void SquareSquare()
+        {
+        }
+
+        public void TriangleSquare()
+        {
+        }
+    }
+}
---
+++ practise_task1/practise_task1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace practise_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
