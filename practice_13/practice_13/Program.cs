0e27bc04906a5b8623fe18b01dbe46bf255ac416
Hanna Piatrouskaya <anka.petrovskaya@gmail.com>
2021-03-22 19:43:29 +0300

    added practice 13

---
+++ practice_13/practice_13/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace practice_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UniqueItem.Id = int.MinValue;
+            UniqueItem.Id = int.MinValue+1;
+        }
+    }
+}
